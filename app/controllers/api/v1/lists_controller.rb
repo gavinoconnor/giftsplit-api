17886e268d959d1cfa1aca7e5106aa159926ffcb
gavinoconnor <gedmund@gmail.com>
2021-03-06 17:33:24 -0500

    add CRUD actions to list controller

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,22 +1,36 @@
 class Api::V1::ListsController < ApplicationController
+  
+  
   def index
     @lists = List.all 
     render json: @lists
   end
 
+  def show
+    @list = List.find(params[:id])
+    render json: @list 
+  end
+
+  def create 
+    @list = List.create(list_params)
+    render json: @list
+  end
+
   def update
+    @list = List.find_by(id: params[:id])
     @list.update(list_params)
-    if @list.save
-      render json: @list, status: :accepted
-    else
-      render json: { errors: @list.errors.full_messages }, status: :unprocessible_entity
-    end
+    render json: @list
+  end
+
+  def destroy 
+    @list = List.find(params[:id])
+    List.destroy(@list)
   end
 
   private
 
-  def list_params
-    params.permit(:title, :description)
-  end
-  
+    def list_params
+      params.require(:list).permit(:title, :description)
+    end
+
 end
